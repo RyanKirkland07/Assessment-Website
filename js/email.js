9706daeadbbe934d7fcf39a4c37ca2ca98f0771b
RyanKirkland07 <kryan8574@gmail.com>
2026-05-27 15:43:34 +1000

    added required labels for required fields in the form and restructured form outputs

--- js/email.js
+++ js/email.js
@@ -7,13 +7,29 @@ RESET.addEventListener('click', resetForm);
 function processForm(event){
     let form = document.querySelector("#email-form");
 
-    let p = document.querySelector("#paragraph");
-    let radio = document.querySelector('input[name="age-range"]:checked').value;
-    let checkbox = document.querySelectorAll('input[name="sector"]:checked');
-    p.textContent += form.elements['fname'].value + " " + form.elements['lname'].value + " " + form.elements['email'].value + " " + radio + " ";
-    for(let i = 0; i < checkbox.length; i++){
-        p.textContent += checkbox[i].value + " ";
+    let p = document.querySelector("#name");
+    p.textContent = `Hello, ${form.elements['fname'].value} ${form.elements['lname'].value}`;
+
+    p = document.querySelector("#email");
+    p.textContent = `You signed up for the Game Software newsletter with the email ${form.elements['email'].value}`;
+
+    p = document.querySelector("#age");
+    let ageRange = document.querySelector('input[name="age-range"]:checked').value;
+    p.textContent = `You said you are in the ${ageRange} age range`;
+    
+    p = document.querySelector("#sectors");
+    let sectorBoxes = document.querySelectorAll('input[name="sector"]:checked');
+    let sectorText = `You selected news for the sectors: `;
+    for(let sector of sectorBoxes){
+        sectorText += sector.value + ', ';
     }
+    if(sectorBoxes.length == 0){
+        sectorText += 'no sectors selected';
+    }
+    p.textContent = sectorText.replace(/, $/, "");
+
+    p = document.querySelector("#additional-notes");
+    p.textContent = `Additional Notes: ${form.elements['additional-text'].value}`
 }
 
 function resetForm(event){
