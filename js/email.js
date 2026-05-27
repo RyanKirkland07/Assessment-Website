bf43dce91c507760504274aa0161bd1a47c0fae8
RyanKirkland07 <kryan8574@gmail.com>
2026-05-27 16:38:26 +1000

    made submitting form alert value and then close window

--- js/email.js
+++ js/email.js
@@ -5,8 +5,7 @@ SUBMIT.addEventListener('click', processForm);
 RESET.addEventListener('click', resetForm);
 
 function processForm(event){
-    event.preventDefault();
-
+    let out = ``;
     let form = document.querySelector("#email-form");
 
     let fname = form.elements["fname"].value;
@@ -15,14 +14,17 @@ function processForm(event){
     let ageRange = document.querySelector("input[name='age-range']:checked");
     if(fname == "" || lname == "" || email == "" || ageRange == null){
         alert("Please Provide Input For All Required Fields");
+        event.preventDefault();
         return false;
     }
 
     let p = document.querySelector("#name");
+    out += `Hello, ${fname} ${lname}\n`;
     p.textContent = `Hello, ${fname} ${lname}`;
 
     p = document.querySelector("#email");
-    p.textContent = `You signed up for the Game Software newsletter with the email ${email}`;
+    out += `You signed up for the Game Software newsletter with the email: ${email} \n`;
+    p.textContent = `You signed up for the Game Software newsletter with the email: ${email}`;
 
     p = document.querySelector("#age");
     p.textContent = `You said you are in the ${ageRange.value} age range`;
@@ -33,15 +35,22 @@ function processForm(event){
     for(let sector of sectorBoxes){
         sectorText += sector.value + ', ';
     }
+    out += sectorText + `\n`;
     if(sectorBoxes.length == 0){
         sectorText += 'no sectors selected';
     }
     p.textContent = sectorText.replace(/, $/, "");
 
     p = document.querySelector("#additional-notes");
+    out += `Additional Notes: ${form.elements['additional-text'].value}`
     p.textContent = `Additional Notes: ${form.elements['additional-text'].value}`
+
+    alert(out);
+
+    window.close();
 }
 
 function resetForm(event){
+    event.preventDefault();
     document.querySelector("#email-form").reset();
 }
