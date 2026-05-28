3b7605fcaddb5762e7ec31e186e21204e7c47b58
RyanKirkland07 <kryan8574@gmail.com>
2026-05-28 22:34:41 +1000

    removed unused constants

--- js/JavascriptExamplesHandler.js
+++ js/JavascriptExamplesHandler.js
@@ -1,5 +1,3 @@
-const ETCH = document.querySelector("#etch");
-const RPS = document.querySelector("#rps");
 const FRAME = document.querySelector("#js-example-frame");
 
 FRAME.focus();
@@ -7,6 +5,3 @@ FRAME.focus();
 function changeExample(event){
     FRAME.setAttribute("src", event.target.value);
 }
-
-ETCH.addEventListener('click', changeExample);
-RPS.addEventListener('click', changeExample);
